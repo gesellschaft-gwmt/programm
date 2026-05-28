9f13e921dee92f4b53ad580a5a42b54febd1c037
LustigerLeo <99617083+LustigerLeo@users.noreply.github.com>
2026-05-28 15:45:56 +0200

    umfeld (+js) neu beschrieben

--- js/umfeld.js
+++ js/umfeld.js
@@ -12,16 +12,27 @@ const translations = {
 
     dk_day_1: "08/09/2026",
     dk_day_2: "09/09/2026",
-    
-    day_gwmtmv: "",
-    time_gwmtmv: "",
-    event_gwmtmv: "",
-
-
-
-
-
-    
+    day_gwmtmv:"09/09/2026",
+    time_gwmtmv:"03:30 p.m. - 06:30 p.m.",
+    event_gwmtmv:"GWMT General Assembly",
+    rt_day: "10/09/2026",
+    rt_time:"01:30 p.m. - 03:00 p.m.",
+    ec_day:"10/09/2026",
+    ec_time:"05:15 p.m. - 06:00 p.m.",
+    agf_day:"10/09/2026",
+    agf_time:"01:30 p.m. 03:00 p.m.",
+    day_pecee:"10/09/2026",
+    time_pecee:"05:15 p.m. - 06:00 p.m.",
+    guidedtours: "guidedtours",
+    day_gt:"Day",
+    time_gt:"Time",
+    event_gt:"Event",
+    location_gt:"Location",
+    day_gt_1:"09/09/2026",
+    time_gt_1:"01:30 p.m. - 03:00 p.m.",
+    gt_1:"Technical Museum, Prague",
+    day_gt_2:"11/09/2026",
+    time_gt_2:"02:30 p.m. - 03:00 p.m."
   }
 };
 
